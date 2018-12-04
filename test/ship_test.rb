c8a46b9589ac9648d765373ab3e2165c4ee6ff18
Million Bekele <milladem_82@yahoo.com>
2018-12-03 18:12:27 -0700

    Adds tests for hit and sunk?

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -27,9 +27,38 @@ class ShipTest < Minitest::Test
 
   def test_it_starts_with_health_equal_to_length
     ship = Ship.new("Cruiser", 3)
+    ship_2 = Ship.new("Submarine", 2)
 
     assert_equal 3, ship.health
     assert_equal 3, ship.length
+    assert_equal 2, ship_2.health
+    assert_equal 2, ship_2.length
+  end
+
+  def test_getting_hit_reduces_health
+    ship = Ship.new("cruiser", 3)
+
+    assert_equal 3, ship.health
+    ship.hit
+    assert_equal 2, ship.health
+    ship.hit
+    assert_equal 1, ship.health
+
+  end
+
+  def test_new_ship_isnt_sunk
+    ship = Ship.new("Cruiser", 3)
+
+    assert_equal false, ship.sunk?
+  end
+
+  def test_it_was_hit_and_the_ship_sank
+    ship = Ship.new("cruiser", 3)
+
+    assert_equal 3, ship.health
+    3.times { ship.hit }
+    assert_equal 0, ship.health
+    assert_equal true, ship.sunk?
   end
 
 end
