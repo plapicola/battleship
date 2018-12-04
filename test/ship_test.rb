dd1ac7b7ced815f3f9d52c3a6a51d72e0d0bc146
Million Bekele <milladem_82@yahoo.com>
2018-12-03 17:39:47 -0700

    Adds ship name, length, health + tests

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -4,8 +4,32 @@ require 'minitest/pride'
 
 class ShipTest < Minitest::Test
   def test_that_it_exists
-    ship = Ship.new
+    ship = Ship.new("Cruiser", 3)
 
     assert_instance_of Ship, ship
   end
+
+  def test_it_has_a_name
+    ship = Ship.new("Cruiser", 3)
+    ship_2 = Ship.new("Submarine", 2)
+
+    assert_equal "Cruiser", ship.name
+    assert_equal "Submarine", ship_2.name
+  end
+
+  def test_it_has_a_length
+    ship = Ship.new("Cruiser", 3)
+    ship_2 = Ship.new("Submarine", 2)
+
+    assert_equal 3, ship.length
+    assert_equal 2, ship_2.length
+  end
+
+  def test_it_starts_with_health_equal_to_length
+    ship = Ship.new("Cruiser", 3)
+
+    assert_equal 3, ship.health
+    assert_equal 3, ship.length
+  end
+
 end
