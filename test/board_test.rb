dade7d1a37bbbe3d536e557669715baa859f33b7
plapicola <p.lapicola@gmail.com>
2018-12-04 17:50:01 -0700

    Adds tests for ability to place ships

--- test/board_test.rb
+++ test/board_test.rb
@@ -59,4 +59,20 @@ class BoardTest < Minitest::Test
    assert board.valid_placement?(submarine, ["A1", "B1"])
  end
 
+ def test_has_ability_to_place_ships
+   board = Board.new
+   cruiser = Ship.new("Cruiser", 3)
+
+   assert board.valid_placement?(cruiser, ["A1", "B1", "C1"])
+   board.place(cruiser, ["A1", "A2", "A3"])
+
+   cell_1 = board.cells["A1"]
+   cell_2 = board.cells["A2"]
+   cell_3 = board.cells["A3"]
+
+   assert_equal cell_1.ship, cruiser
+   assert_equal cell_1.ship, cell_2.ship
+   assert_equal cell_1.ship, cell_3.ship
+ end
+
 end
