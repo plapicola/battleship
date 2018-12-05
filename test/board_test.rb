3956d2dfaef27e8980e633b0782816c15bd7368b
plapicola <p.lapicola@gmail.com>
2018-12-04 17:15:26 -0700

    Adds test for verification of cells generated by board

--- test/board_test.rb
+++ test/board_test.rb
@@ -12,4 +12,13 @@ class BoardTest < Minitest::Test
     assert_instance_of Board, board
   end
 
+  def test_board_is_made_of_cells
+    board = Board.new
+
+    assert_equal 16, board.keys.length
+    board.cells.each do |key, value|
+      assert_instance_of Cell, value
+    end
+ end
+
 end
