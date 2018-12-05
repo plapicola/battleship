8334f25ef25e3c7dbea18d5f040948279de72e1e
plapicola <p.lapicola@gmail.com>
2018-12-04 17:14:06 -0700

    Adds board test file and board class file

---
+++ test/board_test.rb
@@ -0,0 +1,9 @@
+require_relative '../lib/board'
+require_relative '../lib/cell'
+require_relative '../lib/ship'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class BoardTest < Minitest::Test
+
+end
